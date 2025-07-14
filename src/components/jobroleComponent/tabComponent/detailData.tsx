753b05e9cc6d25e633e0018484696fb1d35d7a57
robertparkar99 <133192288+robertparkar99@users.noreply.github.com>
2025-07-14 18:25:14 +0530

    search department as per nidustries

--- src/components/jobroleComponent/tabComponent/detailData.tsx
+++ src/components/jobroleComponent/tabComponent/detailData.tsx
@@ -292,9 +292,6 @@ experience */}
               value={formData.experience}
             />
           </div>
-{/* education
-training
-experience */}
         </div>
 
         <button type="submit" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
