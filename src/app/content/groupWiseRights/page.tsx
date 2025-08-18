ee3b8b0bd95d8ab1579c1f41b4a5af45bd475c0a
Vivek <vivekgajera2709@gmail.com>
2025-08-18 17:39:03 +0530

    Right management frontend work done

---
+++ src/app/content/groupWiseRights/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+import { RightsManagement } from './create/RightsManagement';
+
+
+export default function GroupWiseRightsPage() {
+  return (
+    <>
+      <RightsManagement/>
+    </>
+  );
+}
