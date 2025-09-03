0efe3150bdcd6fd49d7329ffa4d57bc3ece852eb
Vivek <vivekgajera2709@gmail.com>
2025-09-03 12:44:55 +0530

    ability library frontend and api work done

--- src/app/content/Libraries/skillLibraryv2.tsx
+++ src/app/content/Libraries/skillLibraryv2.tsx
@@ -15,7 +15,7 @@ import BehaviourTaxonomy from "../Libraries/BehaviourTaxo";
 import KnowledgeLibrary from "../Knowledge_library/page";
 import Behaviour from "../Behaviour-library/page";
 import Attitude from "../Attitude-library/page";
-
+import Ability from "../ability-library/page";
 // ✅ Loader Component
 const Loader = () => (
   <div className="flex justify-center items-center h-screen">
@@ -96,7 +96,7 @@ const SkillLibrary = () => {
             {activeTab === "Skill Library" && <DynamicSkill />}
             {activeTab === "Jobrole Library" && <DynamicJobrole />}
             {activeTab === "Knowledge" && <KnowledgeLibrary />}
-            {activeTab === "Ability" && <h1 className="text-center">Coming soon</h1>}
+            {activeTab === "Ability" && <Ability/>}
             {activeTab === "Attitude" && <Attitude />}
             {activeTab === "Behaviour" && <Behaviour />}
           </>
