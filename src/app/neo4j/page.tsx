5b3878a0400007e77c79501c90d1cc36a5c39142
AJprSINGH <ajit.12008255@lpu.in>
2025-10-09 00:00:39 +0530

    scrollin issue on neo4j solved

--- src/app/neo4j/page.tsx
+++ src/app/neo4j/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 const Index = () => (
     <iframe
         src="https://skill-ontology-neo4j.vercel.app/"
-        style={{ width: '100%', height: '500%', border: 'none' }}
+        style={{ width: '100%', height: '100%', border: 'none' }}
         title="Skill Neo4j"
     />
 );
